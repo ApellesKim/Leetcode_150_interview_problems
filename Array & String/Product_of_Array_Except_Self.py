2ea6e6f6695f86863dfcd7860082cdae872615e2
ApellesKim <90055859+ApellesKim@users.noreply.github.com>
2024-12-24 09:58:54 +0000

    day 1 complete

--- Array & String/Product_of_Array_Except_Self.py
+++ Array & String/Product_of_Array_Except_Self.py
@@ -26,3 +26,46 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
 
 Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
 """
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        #Day 1 approach (!8/24 testcases passed : time limit exceeded)
+        """
+        First thought about solution is to create list ans and to use double for loop to append products into the list.
+
+        1. Initialize an empty list ans.
+        2. Create a for loop which loops through all of the elements in the array nums.
+            - Initialize product = 0. -> This should be fixed. Since 0 multiplies any number is 0, so I can't find the product of the elements. This can just be procut = 1.
+        3. Create another for loop inside.
+            - This for loop will also loop through all of the elements in the array nums.
+            - Multiply every element except self everytime it loops and update product.
+        4. After inner for loop is completed, append product to list ans.
+        5. Return ans.
+
+        The problem occurs here. [0,0]
+        My condition of inner loop skips when nums[i] == nums[j]. When elements in the list are the same, it not only skips itself but other elements too. 
+        So, I think that I should use another condition to skip itself.
+        This can be solved by changing condition to i == j.
+
+        Another problem occured. Time limit exceeded when nums's length is huge.
+        I have no idea about this right now.
+        Double for loop is O(n^2) in space complexity, so it might have problem when input is large. So, I better to use another algorithm to solve this problem.
+        """
+        ans = []
+        n = len(nums)
+
+        for i in range(n):
+            product = 1
+            for j in range(n):
+                if j == i:
+                    continue
+                product = product * nums[j]
+                
+            ans.append(product)
+
+        return ans
